4680d8f275bae0a1338512d9b7d8aeda1776cc02
Shrinath Shankar <shrinathshankar11@gmail.com>
2022-05-18 12:16:55 -0500

    MATCH: ui started

---
+++ src/main/ui/model/types.ts
@@ -0,0 +1,12 @@
+type team = {
+    playerOne: player
+    playerTwo: player
+    win: boolean
+}
+
+type player = {
+    name: String
+    username: String
+    password: String
+    rating: number
+}
