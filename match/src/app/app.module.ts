40bc1cd44e8d5ca342c989cc1203f644f35ac642
Shrinath Shankar <shrinathshankar11@gmail.com>
2022-05-22 12:07:06 -0500

    MATCH: created ui request, need to test

--- match/src/app/app.module.ts
+++ match/src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import {AppRoutingModule, routingComponent} from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule} from "@angular/common/http";
+import {FormsModule} from "@angular/forms";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    routingComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
