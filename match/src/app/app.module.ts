faf670003621dbc3769704f7121d00d0662a3e7e
Shrinath Shankar <shrinathshankar11@gmail.com>
2022-05-30 14:59:00 -0500

    MATCH: added username localstorage, routing to homepage

--- match/src/app/app.module.ts
+++ match/src/app/app.module.ts
@@ -8,6 +8,7 @@ import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {SignInComponent} from "./sign-in/sign-in.component";
 import {SignUpComponent} from "./sign-up/sign-up.component";
 import { HomePageComponent } from './home-page/home-page.component';
+import { LadderComponent } from './ladder/ladder.component';
 
 
 @NgModule({
@@ -16,7 +17,8 @@ import { HomePageComponent } from './home-page/home-page.component';
     SignInComponent,
     SignUpComponent,
     routingComponent,
-    HomePageComponent
+    HomePageComponent,
+    LadderComponent
   ],
     imports: [
         BrowserModule,
